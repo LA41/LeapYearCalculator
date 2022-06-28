3abc6ad0abb51ac6ac060e7b8ecc16309382134e
Lukasz Adamczyk <lu.adamczyk97@gmail.com>
2022-06-28 13:00:43 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         //checking year divisible by 4, 100 and 400
-        boolean l = LeapYearCalculator.isLeapYear(1200);
-        System.out.println(l);
+        boolean isDivisibleByFourAndOneHundredAndFourHundred = LeapYearCalculator.isLeapYear(1200);
+        System.out.println(isDivisibleByFourAndOneHundredAndFourHundred);
         //checking year divisible by 4 and 100
-        l = LeapYearCalculator.isLeapYear(1100);
-        System.out.println(l);
-        //checking year divisible by < 1 or > 9999
-        l = LeapYearCalculator.isLeapYear(123454);
-        System.out.println(l);
+        boolean isDivisibleByFourAndFourHundred = LeapYearCalculator.isLeapYear(1100);
+        System.out.println(isDivisibleByFourAndFourHundred);
+        //checking if year is between 1 and 9999
+        boolean isWithinRange = LeapYearCalculator.isLeapYear(123454);
+        System.out.println(isWithinRange);
 
 
     }
